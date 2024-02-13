615619e5287e13ef26372a2fea829ea6ee95c791
clarspau <125636195+clarspau@users.noreply.github.com>
2024-02-12 20:59:23 -0800

    modified middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -43,7 +43,47 @@ function ensureLoggedIn(req, res, next) {
 }
 
 
+/** Middleware to use when they have to be logged in as an admin.
+ * 
+ * If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+
+/** Middleware to use to ensure that the request is made by the user whose username matches the one provided as a route parameter,
+ * 
+ * must also provide a valid token.
+ * 
+ * If not, raises UnauthorizedError.
+ */
+
+function ensureCorrectUserOrAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+
+    if (!(user && (user.isAdmin || user.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUserOrAdmin
 };
