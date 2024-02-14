142f9f1ecab0d5effff41ae8c555ec929d6b8e4e
clarspau <125636195+clarspau@users.noreply.github.com>
2024-02-13 21:46:56 -0800

    renamed file: routes/job.js -> routes/jobs.js

--- routes/users.test.js
+++ routes/users.test.js
@@ -5,6 +5,7 @@ const request = require("supertest");
 const db = require("../db.js");
 const app = require("../app");
 const User = require("../models/user");
+const Job = require("../models/job");
 
 const {
   commonBeforeAll,
